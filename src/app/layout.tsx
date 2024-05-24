7e9a9bff5401c2e35e47459b2f006a249c49f9cd
John Jezon Barlisan Ajias <johnjezonajias@gmail.com>
2024-05-24 13:18:55 +0800

    Add: Font family setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
+import { Quicksand } from "next/font/google";
 
 import "@/styles/globals.css"
 import PreLoader from "@/components/layout/PreLoader";
 
-const poppins = Poppins({ 
-  weight: ["300", "400", "500", "600", "700", "900"],
-  style: ["normal", "italic"],
+const quicksand = Quicksand({ 
+  weight: ["300", "400", "500", "600", "700"],
+  style: ["normal"],
   subsets: ["latin"],
   display: "swap",
 });
@@ -23,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <PreLoader font={poppins.className}>
+      <PreLoader font={quicksand.className}>
         <main className="flex min-h-screen flex-col items-center justify-between p-24">{children}</main>
       </PreLoader>
     </html>
