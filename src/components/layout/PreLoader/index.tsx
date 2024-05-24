934b046af2167640d9e5bd1b62dee9b2e06083db
John Jezon Barlisan Ajias <johnjezonajias@gmail.com>
2024-05-24 10:55:36 +0800

    Add: Splash screen component

--- src/components/layout/PreLoader/index.tsx
+++ src/components/layout/PreLoader/index.tsx
@@ -1,7 +1,7 @@
-"use client"
-
-import React, { ReactNode, useEffect, useState } from "react"
+import React, { ReactNode} from "react"
 import { SplashScreen } from "./SplashScreen";
+
+import styles from "./SplashScreen.module.css";
 
 type PreLoaderType = {
     font: string,
@@ -9,27 +9,12 @@ type PreLoaderType = {
 }
 
 export default function PreLoader({font, children}: PreLoaderType) {
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        // Simulate loading delay (remove in production)
-        const timer = setTimeout(() => {
-            setLoading(false);
-        }, 2000); // Adjust the delay as needed
-    
-        // Clean up the timer
-        return () => clearTimeout(timer);
-    }, []);
-
     return (
         <body className={font}>
-            {loading ? (
+            <div className={styles.preloaderContainer}>
                 <SplashScreen />
-            ) : (
-                <>
-                    {children}
-                </>
-            )}
+                {children}
+            </div>
         </body>
     )
 }
